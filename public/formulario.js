8e17f7e7d077e0b6fa74abb444e858994dd93cfb
ramonespier <ramon.coelhomelo@outlook.com>
2024-06-14 08:46:41 -0300

    feat: input ID readonly

--- public/formulario.js
+++ public/formulario.js
@@ -1,18 +1,14 @@
-// <button type="button" id="btAdd">Adicionar</button>
-const btAdd = document.createElement('button');
-btAdd.type = 'button';
-btAdd.id = 'btAdd';
-btAdd.innerHTML = 'Adicionar';
+
 
 // <input type="text" id="preco">
 
 const inputs = [
-    { "type": "text", "id": "idProduto", "label": "ID" },
-    { "type": "text", "id": "descricao", "label": "Descrição" },
+    { "type": "text", "id": "idProduto", "label": "ID", "readOnly": true },
+    { "type": "text", "id": "descricao", "label": "Descrição", "autofocus": true },
     { "type": "text", "id": "preco", "label": "Preço" }
 ];
 
-function criaDivForm (inputs) {
+function criaDivForm(inputs) {
 
     const divForm = document.createElement('div');
     divForm.classList.add('form');
@@ -21,27 +17,33 @@ function criaDivForm (inputs) {
 
         const div = document.createElement('div')
         div.classList.add('controle-form');
-    
+
         const i = document.createElement('input');
         i.type = produto.type;
         i.id = produto.id;
         i.placeholder = produto.label;
-    
+        i.readOnly = produto.readOnly ?? false
+        i.autofocus = produto.autofocus ?? false
+
         // const l = document.createElement('label')
         // l.for = produto.id;
         // l.innerHTML = produto.label;
-    
+
         div.append(i);
         divForm.append(div);
     });
+
+    // <button type="button" id="btAdd">Adicionar</button>
+    const btAdd = document.createElement('button');
+    btAdd.type = 'button';
+    btAdd.id = 'btAdd';
+    btAdd.innerHTML = 'Adicionar';
 
     return divForm;
 }
 
 const divForm = criaDivForm(inputs);
-divForm.append(btAdd);
-
-document.body.append(divForm)
+document.body.append(divForm);
 
 // const divForm = document.createElement('div');
 // divForm.classList.add('form');
